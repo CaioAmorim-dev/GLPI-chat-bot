7ec694b176d245206ce7aab040ed9edfe423d70a
13668497443 <caio13483@gmail.com>
2025-11-12 13:14:27 -0300

    chore: Criar configurações de carregamento de variáveis

--- app/config.py
+++ app/config.py
@@ -1,1 +1,14 @@
-# Variáveis de ambiente (.env)
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    TWILIO_ACCOUNT_SID: str = ""
+    TWILIO_AUTH_TOKEN: str = ""
+    TWILIO_WHATSAPP_NUMBER: str = ""
+    GLPI_URL: str = ""
+    GLPI_APP_TOKEN: str = ""
+    GLPI_USER_TOKEN: str = ""
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
