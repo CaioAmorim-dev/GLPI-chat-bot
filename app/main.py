0eb0de6c4516ca311dc14b513f6f0549d523bf29
13668497443 <caio13483@gmail.com>
2025-11-13 14:35:34 -0300

    refactor: Aplicar uso de wppconnect como bot

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from app.config import settings
+from app.routes import whatsapp 
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"msg": "Bot SDS-PE rodando com sucesso!", "GLPI_URL": settings.GLPI_URL}
+
+# Adiciona o router de WhatsApp
+app.include_router(whatsapp.router)
