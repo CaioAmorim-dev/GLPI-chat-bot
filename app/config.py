01238531c5330b5ae4c5771da6982f052c9cf783
13668497443 <caio13483@gmail.com>
2025-11-14 16:45:53 -0300

    chore: Viabilizar importações de arquivos com __init__

--- app/config.py
+++ app/config.py
@@ -1,12 +1,15 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    TWILIO_ACCOUNT_SID: str = ""
-    TWILIO_AUTH_TOKEN: str = ""
-    TWILIO_WHATSAPP_NUMBER: str = ""
+    TWILIO_ACCOUNT_SID: str
+    TWILIO_AUTH_TOKEN: str
+    TWILIO_WHATSAPP_NUMBER: str
     GLPI_URL: str = ""
     GLPI_APP_TOKEN: str = ""
     GLPI_USER_TOKEN: str = ""
+    
+    OLLAMA_URL: str = ""
+    OLLAMA_MODEL: str = ""
 
     model_config = SettingsConfigDict(env_file=".env")
 
